e97b132a133d80d926ead7cac59e8bde585d1ecd
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2023-08-21 21:02:19 -0500

    Use slog for logging

--- cmd/cedd/main.go
+++ cmd/cedd/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"context"
+	"log/slog"
 	"os"
 
 	"github.com/bradenrayhorn/ced/ced"
 	"github.com/bradenrayhorn/ced/env"
 	"github.com/bradenrayhorn/ced/sqlite"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 )
 
 func main() {
@@ -16,18 +15,20 @@ func main() {
 
 	setupLogger(config)
 
-	log.Info().Msg("starting cedd...")
+	slog.Info("starting cedd...")
 
 	pool, err := sqlite.CreatePool(context.Background(), config)
 	if err != nil {
-		log.Fatal().Err(err).Msg("failed to create sqlite pool")
+		slog.Error("failed to create sqlite pool", "error", err)
+		os.Exit(1)
 		return
 	}
 	defer func() { _ = pool.Close(context.Background()) }()
 }
 
 func setupLogger(config ced.Config) {
-	if config.PrettyLog {
-		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	if !config.PrettyLog {
+		h := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{})
+		slog.SetDefault(slog.New(h))
 	}
 }
